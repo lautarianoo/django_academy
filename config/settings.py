cab267551b5bdca76fa2eccb8d02890b4779c41a
lautariano <lautariano777@gmail.com>
2022-02-26 12:27:16 +0500

    fix requirements 2

--- config/settings.py
+++ config/settings.py
@@ -156,6 +156,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_dev'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
@@ -192,11 +195,6 @@ DJOSER = {
      'PASSWORD_RESET_CONFIRM_URL': 'auth/reset/confirm/{uid}/{token}/',
      'TOKEN_MODEL': None
  }
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_dev'),
-
-)
 
 
 EMAIL_USE_TLS = True
