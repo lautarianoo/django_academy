c77f50e2e128cf771dad1053317f55b71d03753d
lautariano <lautariano777@gmail.com>
2022-02-26 12:07:26 +0500

    deploy project

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import datetime
 import os
 from pathlib import Path
+
+import dj_database_url
+
 from .ckeditor import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,7 +115,8 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
